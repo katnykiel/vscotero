75bb03b2187ec9b43a9ca76f3c4513e573d52d17
aerithnetzer <aerithnetzer@outlook.com>
2023-12-28 14:40:21 -0500

    Commit progress

---
+++ vsco-tero.py
@@ -0,0 +1,52 @@
+# vsco-tero
+
+import bibtexparser
+import yaml
+import os
+
+
+def get_bib_database(bibtex_file):
+    """
+    Read a BibTeX file and parse it into a BibDatabase object.
+
+    Args:
+        bibtex_file (str): The path to the BibTeX file.
+
+    Returns:
+        BibDatabase: The parsed BibDatabase object.
+
+    """
+    # Read the bibtex file
+    with open(bibtex_file, 'r') as f:
+        bibtex_str = f.read()
+    
+    # Configure the bibtexparser to allow for non-standard entry types
+    parser = bibtexparser.bparser.BibTexParser(common_strings=True)
+    parser.ignore_nonstandard_types = False
+
+    # Parse the bibtex file
+    bib_database = bibtexparser.loads(bibtex_str, parser=parser)
+
+    return bib_database
+
+def get_yaml_from_bib_entry(bib_entry):
+    """
+    Convert a BibTeX entry to YAML format.
+
+    Args:
+        bib_entry (dict): A dictionary representing a BibTeX entry.
+
+    Returns:
+        str: The YAML representation of the BibTeX entry.
+    """
+    # Convert the entry to yaml
+    yaml_str = yaml.dump(bib_entry)
+
+    print(yaml_str)
+
+    return yaml_str
+
+def loop_through_bib_database(bib_database):
+    
+    for entry in bib_database.entries:
+        get_yaml_from_bib_entry(entry)
