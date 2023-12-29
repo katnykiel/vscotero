3156dd1f80b4d5ee11252b09fe896745406a0fbc
Kat Nykiel <katanykiel@gmail.com>
2023-12-29 00:14:21 -0500

    Add core.py file for BibTeX files and YAML conversion

--- core.py
+++ core.py
@@ -10,19 +10,24 @@ import time
 
 def get_bib_database(bibtex_file):
     """
-    Read a BibTeX file and parse it into a BibDatabase object.
+    Read a BibTeX file and return a BibDatabase object
 
     Args:
         bibtex_file (str): The path to the BibTeX file.
 
     Returns:
-        BibDatabase: The parsed BibDatabase object.
+        bib_database (BibDatabase): A BibDatabase object representing the BibTeX file.
 
     """
+
+    # Check that there is a bib file at this location
+    if not os.path.isfile(bibtex_file):
+        raise FileNotFoundError("No BibTeX file found at {}".format(bibtex_file))
+
     # Read the bibtex file
-    with open(bibtex_file, 'r') as f:
+    with open(bibtex_file, "r") as f:
         bibtex_str = f.read()
-    
+
     # Configure the bibtexparser to allow for non-standard entry types
     parser = bibtexparser.bparser.BibTexParser(common_strings=True)
     parser.ignore_nonstandard_types = False
@@ -31,6 +36,7 @@ def get_bib_database(bibtex_file):
     bib_database = bibtexparser.loads(bibtex_str, parser=parser)
 
     return bib_database
+
 
 def get_yaml_from_bib_entry(bib_entry):
     """
@@ -42,25 +48,42 @@ def get_yaml_from_bib_entry(bib_entry):
     Returns:
         str: The YAML representation of the BibTeX entry.
     """
-    # Convert the entry to yaml
+
+    # Convert the entry to yaml using yaml.dump
     yaml_str = yaml.dump(bib_entry)
 
-    print(yaml_str)
+    # TODO: add checks that we are getting the right YAML elements
 
     return yaml_str
 
+
+def test_functions():
+    """
+    Test the get_bib_database and get_yaml_from_bib_entry functions.
+    """
+
+    # Get the BibDatabase object
+    entries = get_bib_database("tests/test.bib")
+
+    # Print the YAML representation of each BibTeX entry
+    for entry in entries.entries:
+        print(get_yaml_from_bib_entry(entry))
+
+
+test_functions()
+
+
 def loop_through_bib_database(bib_database):
-    
     for entry in bib_database.entries:
         get_yaml_from_bib_entry(entry)
-
 
 
 class FileHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if not event.is_directory:
-            print(f'File {event.src_path} has been modified')
+            print(f"File {event.src_path} has been modified")
             # TODO: Add script executable here
+
 
 def check(watchdog_path):
     path = watchdog_path
@@ -78,5 +101,4 @@ def check(watchdog_path):
     observer.join()
 
 
-
-check()
+# check()
